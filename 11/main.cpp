41ebcd0c2e1c388460a7d9fb3dbb51c0dadeaae5
wangjimjim <110479239+wangjimjim@users.noreply.github.com>
2023-03-19 23:22:36 +0800

    Add files via upload

---
+++ 11/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int rows, cols;
+    cout << "Enter the number of rows and columns: ";
+    cin >> rows >> cols;
+
+    vector<vector<int>> matrix(rows, vector<int>(cols));
+    cout << "Enter matrix elements: " << endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    cout << "Original matrix:" << endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // Reverse the matrix
+    for (int i = 0; i < rows / 2; i++) {
+        for (int j = 0; j < cols; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[rows - i - 1][j];
+            matrix[rows - i - 1][j] = temp;
+        }
+    }
+
+    cout << "Reversed matrix:" << endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
